2caf219578a4ad2e03e8d3bb7e62978ca2cf8b80
Andrew Sinner <andrewsinner@gmail.com>
2014-11-17 16:19:10 -0800

    Add authentication test for category removal

---
+++ test/integration/authorization/timelines/categories/hacker_cannot_delete_categories_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class HackerCannotDeleteCategoriesTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = create_user
+    @company = create_company(@user)
+    @event = create_event(@company)
+    @timeline = create_timeline(@event)
+    @category = create_category(@timeline, Faker::Lorem.sentence)
+    
+    @hacker = create_user('hacker@example.com')
+    sign_in(@hacker)
+    create_company(@hacker)
+  end
+  
+  test 'user can delete timeline categories' do
+    delete "/api/timelines/#{@timeline.id}/categories/#{@category.id}", {
+      token: @hacker.token
+    }.to_json, {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json'
+    }
+    
+    res = json(response.body)
+    assert_equal 403, response.status
+    assert_equal 'You are not authorized for that resource', res[:msg]
+  end
+end
