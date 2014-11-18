57b4b1b43a2c03015f94be410ccd55e2202d6751
Andrew Sinner <andrewsinner@gmail.com>
2014-11-17 17:31:03 -0800

    Add authorization test for timeline event update

--- test/integration/authorization/timelines/events/hacker_cannot_update_timeline_events_test.rb
+++ test/integration/authorization/timelines/events/hacker_cannot_update_timeline_events_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class HackerCannotUpdateTimelineItemsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create_user
+    sign_in(@user)
+    @company = create_company(@user)
+    @event = create_event(@company)
+    @timeline = create_timeline(@event)
+    @category = create_category(@timeline)
+    @timeline_event = create_timeline_event(@category)
+    
+    @hacker = create_user('hacker@example.com')
+    sign_in(@hacker)
+    create_company(@hacker)
+  end
+  
+  test 'user can update timeline events with valid data' do
+    patch "/api/timeline_categories/#{@category.id}/events/#{@timeline_event.id}", {
+      timeline_event: {
+        description: 'My newly updated timeline event'
+      },
+      token: @hacker.token
+    }.to_json, {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json'
+    }
+    
+    res = json(response.body)
+    assert_equal 403, response.status
+    assert_equal 'You are not authorized for that resource', res[:msg]    
+  end
 end
