f8db94f8dbcaf7624a24b06c0c8faa61fa9e42cc
Andrew Sinner <andrewsinner@gmail.com>
2014-11-17 18:46:32 -0800

    Add test for user token removal

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::TokensController < ApplicationController
+  before_action :authenticate_user!, only: [:destroy]
+  
   def create
     user = User.find_by(email: params[:email])
     
@@ -7,6 +9,15 @@ class Api::V1::TokensController < ApplicationController
       render status: 201, json: { token: user.token }
     else
       render status: 401, json: { msg: 'Invalid email or password' }
+    end
+  end
+  
+  def destroy
+    @user = current_user
+    if @user.update(token: nil)
+      render status: 200, json: {msg: 'You have successfully signed out'}
+    else
+      render status: 422, json: {msg: 'There was an error in signing out'}
     end
   end
 end
---
+++ test/integration/tokens/users_can_remove_token_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UserCanRemoveTokenTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = create_user
+    sign_in(@user)
+  end
+  
+  test 'user can remove token with valid credentials' do
+    delete "/api/tokens", { 
+      token: @user.token 
+    }.to_json, {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json'
+    }
+    
+    assert_equal 200, response.status
+    user = User.find(@user.id)
+    assert_nil user.token
+  end
+end
