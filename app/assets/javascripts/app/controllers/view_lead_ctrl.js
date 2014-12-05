9373ad59b5a6c8b14a908304667b9a14eb02d781
Andrew Sinner <andrewsinner@gmail.com>
2014-12-04 16:53:12 -0800

    Add lead tab views

--- app/assets/javascripts/app/controllers/view_lead_ctrl.js
+++ app/assets/javascripts/app/controllers/view_lead_ctrl.js
@@ -4,12 +4,17 @@
 	app.controller('ViewLeadCtrl', ['$scope', 'LEAD_EVENTS', function($scope, LEAD_EVENTS) {
 		
 		$scope.$on(LEAD_EVENTS.viewLead, function(event, data) {
-			$scope.activeTab = 1;
+			$scope.setActiveTab(1);
+			$scope.setCurrentLead(data);
 		});
 		
 		$scope.setActiveTab = function(num) {
 			$scope.activeTab = num;
-		};	
+		};
+		
+		$scope.setCurrentLead = function(lead) {
+			$scope.currentLead = lead;
+		};
 		
 	}]);
 })()
